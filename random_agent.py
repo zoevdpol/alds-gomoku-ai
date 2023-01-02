0c0a92d0c50c1c398671d58612bc54fca0f6e726
Zoe van de Pol <94827713+zoevdpol@users.noreply.github.com>
2023-01-02 14:45:15 +0100

    beste versie woowwwwww

--- random_agent.py
+++ random_agent.py
@@ -19,67 +19,70 @@ class gamestatenode:
         self.valid_moves = valid_moves(self.state)
         self.bestchild = 0 
         self.move = move 
-        self.countpmoves = len(self.valid_moves)     
+        self.countpmoves = len(self.valid_moves)    
+        self.checkselfwon = check_win(self.state[0], self.move) 
 
     def fully_expanded(self):
         return len(self.children) is self.countpmoves
-        
+    
+    
 
-def findSpotToExpand(gamestate: GameState, last_move: Move, n: gamestatenode):
+
+def findSpotToExpand(n: gamestatenode):
     if (n.finished == True) or (n.countpmoves == 0): #if n is terminal (game finished)
         return n
 
     if n.fully_expanded() == False: #if n is not fully expanded
-        temp = deepcopy(n.state)
-        rmove = random.choice(n.valid_moves)
-        #print(rmove)
-        _,_,temp = play(temp, rmove)
-        n.children.append(gamestatenode( temp, n.move, n, rmove)) #child in lijst van children van n zetten
-        n.valid_moves.remove(rmove)
-        return n.children[-1]
+        temp = deepcopy(n.state) #kopie maken van state zodat we hem niet oprecht aanpassen
+        rmove = random.choice(n.valid_moves) #random move kiezen
+        _,_,temp = play(temp, rmove) #kopie van gamestate veranderen door de random move te spelen
+        n.children.append(gamestatenode( temp, n.move, n, rmove)) #child maken en in lijst van children van n zetten
+        n.valid_moves.remove(rmove) #move uit random moves halen
+        return n.children[-1] #hoogste kind returnen
 
-    best_child = n.children[0]
-    maxuct = (best_child.Q / best_child.N) + (0.707 * math.sqrt((2 * math.log(best_child.parent.N, 2)) / best_child.N))
+    best_child = n.children[0] 
+    maxuct = (best_child.Q / best_child.N) + (0.707 * math.sqrt((2 * math.log(best_child.parent.N, 2)) / best_child.N)) 
 
-    for ch in n.children:
-        uct = (ch.Q / ch.N) + (0.707 * math.sqrt((2 * math.log(ch.parent.N, 2)) / ch.N))
-        if uct > maxuct:
+    for ch in n.children: #door alle kinderen heen van n
+        uct = (ch.Q / ch.N) + (0.707 * math.sqrt((2 * math.log(ch.parent.N, 2)) / ch.N)) #uct waarde berekenen
+        if uct > maxuct: #hoogste uct waarde eruit halen
             maxuct = uct
             best_child = ch
-    n.bestchild = best_child 
-    return findSpotToExpand(best_child.state, best_child.move, best_child) #maybe move beter
+
+    n.bestchild = best_child  #beste kind van n in n zetten 
+    return findSpotToExpand(best_child) #weer kijken waar we kunnen expanden bij bestchild
 
 
-def rollout(n: gamestatenode, last_move: Move, black):
-    s = deepcopy(n.state)
-    boolwin = check_win(s[0], last_move)
-    moves = valid_moves(s)
+def rollout(n: gamestatenode, last_move: Move, black): 
+    s = deepcopy(n.state) #kopie van state maken
+    boolwin = check_win(s[0], last_move) 
+    Validmoves = valid_moves(s)
 
-    while not ((boolwin) or (len(moves) <= 0)):
-        a = random.choice(moves)
+    while not ((boolwin) or (len(Validmoves) <= 0)): #geen terminal moves (geen winnende move en lengte van validmoves is nog hoger dan 0)
+        a = random.choice(Validmoves) #random move uitkiezen
         last_move = a 
-        _,_,s = play(s, a)
-        moves = valid_moves(s)
-        boolwin = check_win(s[0], last_move)
+        _,_,s = play(s, a) #random move spelen 
+        Validmoves = valid_moves(s) #validmoves lijst weer updaten
+        boolwin = check_win(s[0], last_move) #checkwin weer updaten
 
-    if (boolwin == False) and len(moves) == 0:
-        return 0.5
+    if (boolwin == False) and len(Validmoves) == 0: #gelijkspelen
+        return 0
       
-    if (boolwin == True) and  ((s[1] % 2) == 0 ):
-        if not black:
-            return 1
+    if (boolwin == True) and  ((s[1] % 2) == 0 ): 
+        if not black: 
+            return 1 #win
         else:
-            return 0
+            return -1 #verlies
     
     if (boolwin == True) and ((s[1] % 2) != 0):
         if not black:
-            return 0
+            return -1 #verlies
         else:
-            return 1
+            return 1 #win
     
 
 def Backupvalue(n, val, black):
-    while n is not None:
+    while n is not None: #zolang we niet bij de root zijn
         n.N = n.N + 1
         if n.state[1] % 2 != 0:
             if black:
@@ -101,31 +104,24 @@ class random_dummy_player:
         self.black = black_
 
 
-    def move(
-        self, state: GameState, last_move: Move, max_time_to_move: int = 1000
-    ) -> Move:
-        node = gamestatenode(state, last_move, None)
+    def move(self, state: GameState, last_move: Move, max_time_to_move: int = 1000) -> Move:
+        rootnode = gamestatenode(state, last_move, None)
         starttime = time.time()
-        ding_count = 0
-        while time.time() < starttime + 10:
-            nleaf = findSpotToExpand(state, last_move, node)
-            value = rollout(nleaf, last_move, self.black)
-            Backupvalue(nleaf, value, self.black)
-            ding_count += 1
-
-        best_child = node.children[0]
-        maxuct = best_child.Q / best_child.N
-        for ch in node.children:
+        while time.time() < starttime + 1: #zolang de tijd niet 0 is
+            nleaf = findSpotToExpand(rootnode) #nlead maken
+            value = rollout(nleaf, last_move, self.black) #waarde van nleaf berekenen
+            Backupvalue(nleaf, value, self.black) #backupvalues berekenen
+            
+        best_child = rootnode.children[0]
+        maxuct = best_child.Q / best_child.N 
+        for ch in rootnode.children: #door alle children van de rootnode
+            if ch.checkselfwon == True: #als ik gewonnen heb met de volgende move dan die altijd spelen
+                return ch.move
             uct = (ch.Q / ch.N) 
-            print("MOVE: ",ch.move," UCT: ",uct)
-            print("Q: ", ch.Q)
-            print("N: ", ch.N)
-            print("                                                                     ")
-            if uct > maxuct:
+            if uct > maxuct: #anders gewoon de child met de beste uct waarde pakken en de move daarvan berekenen
                 maxuct = uct
                 best_child = ch
-        node.bestchild = best_child 
-        print(ding_count)
+        rootnode.bestchild = best_child 
         return best_child.move
         
         
